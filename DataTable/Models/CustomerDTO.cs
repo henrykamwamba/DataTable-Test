799b9624c4cfaa1962dc5ac87ebdaad724e4e02a
henry.kamwamba <henry.kamwamba@angledimension.com>
2022-05-19 20:44:55 +0200

    Made code changes

--- DataTable/Models/CustomerDTO.cs
+++ DataTable/Models/CustomerDTO.cs
@@ -1,8 +1,11 @@
-﻿namespace DataTable.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataTable.Models
 {
     public class CustomerDTO
     {
         public int CustomerId { get; set; }
+        [Display(Name = "Customer Name")]
         public string CustomerName { get; set; }
         public string Gender { get; set; }
     }
