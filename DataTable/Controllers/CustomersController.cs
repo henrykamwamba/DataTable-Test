d4f6c0d7913dbc34200851b23c19cf1c2ed78a8d
henry.kamwamba <henry.kamwamba@angledimension.com>
2022-05-19 20:37:22 +0200

    Add project files.

---
+++ DataTable/Controllers/CustomersController.cs
@@ -0,0 +1,37 @@
+﻿using DataTable.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace DataTable.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(GetAllCustomers());
+        }
+        
+        public List<CustomerDTO> GetAllCustomers()
+        {
+            var customers = new List<CustomerDTO>
+            {
+                new CustomerDTO
+                {
+                    Gender = "Male",
+                    CustomerId = 1,
+                    CustomerName = "Henry Kamwamba"
+                }
+            };
+            Response.StatusCode = customers.Any() ? (int)HttpStatusCode.OK : (int)HttpStatusCode.BadRequest;
+            return customers;
+        }
+        
+        public JsonResult GetCustomers()
+        {
+            return Json(GetAllCustomers());
+        }
+    }
+}
+
---
+++ DataTable/Models/CustomerDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace DataTable.Models
+{
+    public class CustomerDTO
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string Gender { get; set; }
+    }
+}
